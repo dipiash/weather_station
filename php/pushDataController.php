cc7819763b91ac4fbbbdfe1027420675b6cf1fb5
Dmitriy <dipiash@users.noreply.github.com>
2023-09-10 17:52:40 +0300

    Update pushDataController.php

--- php/pushDataController.php
+++ php/pushDataController.php
@@ -1,24 +1,18 @@
 <?php
 
-# вставка данных пришедших с дачиков в БД
 function pushData($type, $value) {
-    # проверям, что `value` число
     if (!is_numeric($value)) {
-        return array('error' => 1, 'msg' => 'Неверный тип данных в `value`.');
+        return array('error' => 1, 'msg' => 'Incorrect data type in `value`.');
     }
 
-    # подключаем функцию соединения с БД и выбора коллекции
     require_once('functions/db_connect.php');
-    # подключаем функцию вставки данных в БД
     require_once('functions/insertData.php');
 
     $connection = createDbConnect();
 
-    # выбираем коллекцию
     $collection = choseCollection($connection, 'weth', 'weth', date('j'), date('m'), date('y'));
     if (is_object($collection)) {
 
-        # записываем данные в коллекцию
         $insData = insertData($collection, $type, $value);
         if (isset($insData['error'])) {
 
@@ -29,7 +23,6 @@ function pushData($type, $value) {
 
         $connection->close();
 
-        # если все прошло успешно
         return 1;
     }
 
